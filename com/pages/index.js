26d6ce0329febd27d4799ea0e0d9a9ebe196b010
tecshuttle <tecshuttle@gmail.com>
2022-01-22 21:47:06 +0800

    bit add header and footer components.

--- com/pages/index.js
+++ com/pages/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import {Header} from '@tecshuttle/mik.shared.header'
+import {Footer} from '@tecshuttle/mik.shared.footer'
 
 export default function Home() {
   return (
@@ -10,10 +12,15 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Michaels.com
-        </h1>
+        <Header text="H"/>
+        <div flex="1">
+          <h1 className={styles.title}>
+            Michaels.com
+          </h1>
+        </div>
+        <Footer />
       </main>
+
     </div>
   )
 }
